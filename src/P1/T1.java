9d64634c248b98ca6ca8684fd5446d795ce62a42
Nivedita Gavade <nivedita.gavade19@gmail.com>
2023-05-22 13:20:27 +0530

    project upload

---
+++ src/P1/T1.java
@@ -0,0 +1,9 @@
+package P1;
+
+public class T1 {
+	
+
+public static void main(String[] args) {
+	System.out.println("T1.java file1");
+}
+}
---
+++ src/P2/T2.java
@@ -0,0 +1,10 @@
+package P2;
+
+public class T2 {
+	public static void main(String[] args) {
+		System.out.println("T2.java file1");
+		System.out.println("T2.java file2");
+	}
+	
+
+}
